5e0e538dd130513654e5df7f478f3ac0263079ff
imshaz <imshazbangash@gmail.com>
2021-06-05 14:59:26 +0500

    created main styled component

---
+++ src/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+
+export const AppContainer= styled.div`
+background-color:#3179ba; 
+display:flex; 
+align-items:flex-start;
+flex-direction:row; 
+height:100%; 
+padding:20px; 
+width:100%;
+`
+
+export const ColumnContainer=styled.div`
+max-width: 300px;
+background-color: #ebecf0;
+min-height: 40px;
+margin-right: 20px;
+border-radius: 3px;
+padding: 8px 8px;
+flex-grow: 0;
+`
+
+
+export const ColumnTitle=styled.div`
+padding: 6px 16px 12px;
+font-weight: bold;
+`
+export const CardContainer=styled.div`
+background-color: #fff;
+cursor: pointer;
+margin-bottom: 0.5rem;
+padding: 0.5rem 1 rem;
+max-width: 300px;
+width: 300px;
+box-shadow: #091e4240 0px 1px 0px 0px;
+`
+
